6fca34d7f345dd6ce0667f0c0ef1ae3f68461c15
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-21 12:18:22 +0100

    Sobota

--- 5.py
+++ 5.py
@@ -9,7 +9,11 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
-xgb_classifier = xgb.XGBClassifier(random_state=42)
+xgb_classifier = xgb.XGBClassifier(
+    n_estimators=1000,
+    max_depth=3,
+    learning_rate=0.3,
+    random_state=42)
 xgb_classifier.fit(X_train, y_train)
 
 train_acc = xgb_classifier.score(X_train, y_train)
