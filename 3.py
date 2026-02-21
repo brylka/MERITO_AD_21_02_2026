ae2850ac86815a78e7b5201794cc8b3a47c55760
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-21 11:05:00 +0100

    Sobota

--- 3.py
+++ 3.py
@@ -9,3 +9,9 @@ print(digits.images[0])
 plt.imshow(digits.images[0], cmap='gray')
 plt.show()
 
+fig, axes = plt.subplots(2, 5, figsize=(10,5))
+for i, ax in enumerate(axes.flat):
+    ax.imshow(digits.images[i+20], cmap='gray')
+    ax.set_title(f"Cyfra: {digits.target[i+20]}")
+    ax.axis('off')
+plt.show()
