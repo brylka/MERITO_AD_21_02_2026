367da89ab5a008fc8fbfb876287b20d96e692e1d
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-21 09:01:28 +0100

    Sobota

---
+++ 1.py
@@ -0,0 +1,20 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+single_tree = DecisionTreeClassifier(random_state=42)
+single_tree.fit(X_train, y_train)
+
+train_acc = single_tree.score(X_train, y_train)
+test_acc = single_tree.score(X_test, y_test)
+
+print(f"Dokładność na zbiorze treningowym: {train_acc}")
+print(f"Dokładność na zbiorze testowym:    {test_acc}")
+print(f"Różnica:                           {train_acc - test_acc}")
