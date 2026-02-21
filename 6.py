0caccf4ebeb46558657105a6a84d580b5998e4ce
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-21 12:47:15 +0100

    Sobota

--- 6.py
+++ 6.py
@@ -12,22 +12,27 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
+models = {
+    'POJEDYNCZE DRZEWO': DecisionTreeClassifier(random_state=42),
+    'LAS DRZEW DECYZYJNYCH': RandomForestClassifier(random_state=42),
+    'XGBOOST': xgb.XGBClassifier(random_state=42)
+}
 
-single_tree = DecisionTreeClassifier(random_state=42)
-start = time.time()
-single_tree.fit(X_train, y_train)
-stop = time.time()
+for name, model in models.items():
+    start = time.time()
+    model.fit(X_train, y_train)
+    stop = time.time()
 
-tree_train_acc = single_tree.score(X_train, y_train)
-tree_test_acc = single_tree.score(X_test, y_test)
+    tree_train_acc = model.score(X_train, y_train)
+    tree_test_acc = model.score(X_test, y_test)
 
-print("POJEDYNCZE DRZEWO")
-print(f"Dokładność na zbiorze treningowym: {tree_train_acc}")
-print(f"Dokładność na zbiorze testowym:    {tree_test_acc}")
-print(f"Różnica:                           {tree_train_acc - tree_test_acc}")
+    print(f"{name}:")
+    print(f"Dokładność na zbiorze treningowym: {tree_train_acc}")
+    print(f"Dokładność na zbiorze testowym:    {tree_test_acc}")
+    print(f"Różnica:                           {tree_train_acc - tree_test_acc}")
 
-tree_cv_scores = cross_val_score(single_tree, X, y, cv=10)
-print(f"Walidacja krzyżowa:")
-print(f"Średnia dokładność: {tree_cv_scores.mean()}")
-print(f"Odchylenie std:     {tree_cv_scores.std()}")
-print(f"Czas treningu:      {stop - start}")
+    tree_cv_scores = cross_val_score(model, X, y, cv=10)
+    print(f"Walidacja krzyżowa:")
+    print(f"Średnia dokładność: {tree_cv_scores.mean():.2f}")
+    print(f"Odchylenie std:     {tree_cv_scores.std():.2f}")
+    print(f"Czas treningu:      {stop - start:.2f}s")
