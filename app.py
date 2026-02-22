ee54f7a5f6d98658c980ff34667fa69fe5924094
Bartosz Bryniarski <brylka@jelenia.com>
2026-02-22 12:44:48 +0100

    Niedziela

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Cześć!"
+    if request.method == "POST":
+        file = request.files['image']
+        if file:
+            img = Image.open(file).convert('L')
+            img = img.resize((8, 8))
+            data = np.array(img)
+
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
